a09449b2c728d9f4fce736e0190479aa10f0ce2b
Steven Johnson <sjohnson0684@gmail.com>
2023-03-24 10:11:09 -0400

    Implementation of the generic TryResult.

---
+++ src/main/java/com/labofkiwi/TryResult.java
@@ -0,0 +1,62 @@
+package com.labofkiwi;
+
+/**
+ * Represents a result for the Try-Get pattern.
+ *
+ * @param <T> The value's type.
+ */
+public final class TryResult<T> implements ReadOnlyRef<T> {
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private static final TryResult FAILURE = new TryResult(false, null);
+
+    private final boolean isSuccess;
+    private final T       value;
+
+    // Internal constructor.
+    private TryResult(boolean isSuccess, T value) {
+        this.isSuccess = isSuccess;
+        this.value     = value;
+    }
+
+    /**
+     * Gets the resulting value of the process that produced this instance.
+     *
+     * @return The value if successful; otherwise, {@code null}.
+     */
+    @Override
+    public T getValue() {
+        return value;
+    }
+
+    /**
+     * Determines if the process resulting in this instance was successful.
+     *
+     * @return {@code true} is successful; otherwise, {@code false}.
+     */
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    /**
+     * Returns the failure instance, where the value is {@code null} and is not successful.
+     *
+     * @param <T> The value's type.
+     * @return The failure instance.
+     */
+    public static <T> TryResult<T> failure() {
+        @SuppressWarnings("unchecked")
+        TryResult<T> result = (TryResult<T>)FAILURE;
+        return result;
+    }
+
+    /**
+     * Returns a successful instance.
+     *
+     * @param <T> The value's type.
+     * @param value The resulting value.
+     * @return A successful instance.
+     */
+    public static <T> TryResult<T> success(T value) {
+        return new TryResult<>(true, value);
+    }
+}
